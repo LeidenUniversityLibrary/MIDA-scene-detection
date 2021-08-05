2e5cf2f8ed1e3a04471fb9c473e6ea0e503b5664
Ben Companjen <ben@companjen.name>
2021-08-05 17:21:41 +0200

    Make very first frame a scene start
    And delete a column that has timestamps, because they are duplicated

--- scripts/mark_scenes.py
+++ scripts/mark_scenes.py
@@ -14,14 +14,19 @@ csv_dtypes = {
 use_cols = ["frame","pkt_pts_time","lavfi_scd_mafd","lavfi_scd_score","lavfi_scd_time"]
 pivoted_data = pd.read_csv(pivoted_file, dtype = csv_dtypes, index_col="frame", usecols=use_cols)
 
-print(pivoted_data.dtypes)
-# pivoted_data2 = pivoted_data.copy()[["pkt_pts_time", "lavfi_scd_mafd","lavfi_scd_score"]]
+# print(pivoted_data.dtypes)
+
+# Make sure the first frame starts a scene by setting the `scd_time`
+pivoted_data.loc[0, "lavfi_scd_time"] = 0.0
 scene_starts = pivoted_data["lavfi_scd_time"].notna()
 
+# print(pivoted_data.loc[scene_starts, :].reset_index().head())
 
-
-print(pivoted_data.loc[scene_starts, :].reset_index().head())
+# Copy rows that start new scenes and number them by resetting the index
 scenes = pivoted_data.copy().loc[scene_starts, :].reset_index()
 scenes.index.name = "scene"
+# Delete the `scd_time` column as its values are the same as `pkt_pts_time`
+del scenes["lavfi_scd_time"]
+# Save the scenes information to a new file based on the input file name
 analysed_file = pivoted_file.replace(".csv", "-scenes.csv")
 scenes.to_csv(analysed_file)
