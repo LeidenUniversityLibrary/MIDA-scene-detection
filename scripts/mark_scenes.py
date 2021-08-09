aaf725604e795947247c329c6e21003cb2d35273
Ben Companjen <ben@companjen.name>
2021-08-09 16:14:17 +0200

    Output enriched frames and scenes files

--- scripts/mark_scenes.py
+++ scripts/mark_scenes.py
@@ -12,21 +12,43 @@ csv_dtypes = {
     "lavfi_scd_time": "float64"
 }
 use_cols = ["frame","pkt_pts_time","lavfi_scd_mafd","lavfi_scd_score","lavfi_scd_time"]
-pivoted_data = pd.read_csv(pivoted_file, dtype = csv_dtypes, index_col="frame", usecols=use_cols)
-
-# print(pivoted_data.dtypes)
+frames_data = pd.read_csv(pivoted_file, dtype = csv_dtypes, index_col="frame", usecols=use_cols)
 
 # Make sure the first frame starts a scene by setting the `scd_time`
-pivoted_data.loc[0, "lavfi_scd_time"] = 0.0
-scene_starts = pivoted_data["lavfi_scd_time"].notna()
+frames_data.loc[0, "lavfi_scd_time"] = 0.0
 
-# print(pivoted_data.loc[scene_starts, :].reset_index().head())
+# Join the scene indices with the original frame list
 
-# Copy rows that start new scenes and number them by resetting the index
-scenes = pivoted_data.copy().loc[scene_starts, :].reset_index()
+frames_data["lavfi_scd_time"].fillna(method="pad", inplace=True)
+
+scene_numbers = frames_data.groupby("lavfi_scd_time").ngroup()
+print(scene_numbers.head())
+
+frames_with_scenes = frames_data.assign(scene=scene_numbers)
+print(frames_with_scenes.head())
+enriched_file = pivoted_file.replace(".csv", "-with-scenes.csv")
+frames_with_scenes.to_csv(enriched_file)
+
+# Determine length of scenes
+frames_by_scene = frames_data.reset_index().groupby("lavfi_scd_time")
+scene_sizes = frames_by_scene.size()
+print(scene_sizes.head())
+
+frame_duration = frames_data.loc[1, "pkt_pts_time"]
+scene_frames = frames_by_scene["frame"].agg(first_frame="min",
+    last_frame="max")
+print(scene_frames.head())
+
+scenes = frames_by_scene.agg(
+    last_pts=("pkt_pts_time", "max"),
+    duration=("pkt_pts_time", lambda x: round(x.max() - x.min() + frame_duration, 5))
+    )\
+    .assign(number_of_frames=scene_sizes)\
+    .join(scene_frames.loc[:, ["first_frame","last_frame"]])\
+    .reset_index()
 scenes.index.name = "scene"
-# Delete the `scd_time` column as its values are the same as `pkt_pts_time`
-del scenes["lavfi_scd_time"]
+print(scenes.head())
+
 # Save the scenes information to a new file based on the input file name
 analysed_file = pivoted_file.replace(".csv", "-scenes.csv")
 scenes.to_csv(analysed_file)
