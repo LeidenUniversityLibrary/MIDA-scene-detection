abfb3cd0876c33d2541dafa6fb4a3fef2c72bd05
Ben Companjen <ben@companjen.name>
2021-08-23 21:13:15 +0200

    Make filenames more predictable and meaningful

--- flat-to-csv.sh
+++ flat-to-csv.sh
@@ -2,9 +2,10 @@
 # Turn ffprobe's flat format into proper CSV
 
 IN_FILE=$1
-OUT_CSV=${IN_FILE%.txt}.csv
+OUT_CSV=$(dirname $1)/frames-long.csv
+PIVOTED_CSV=$(dirname $1)/frames-wide.csv
 
 echo "frame,variable,value" > ${OUT_CSV}
 sed -E -f $(dirname $0)/scripts/cleanup.txt ${IN_FILE} >> ${OUT_CSV}
 
-python3 $(dirname $0)/scripts/pivot.py ${OUT_CSV}
+python3 $(dirname $0)/scripts/pivot.py ${OUT_CSV} ${PIVOTED_CSV}
--- scdet.sh
+++ scdet.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
 IN_FILE=$1
+OUT_DIR="scene-detection/$2"
+mkdir -p "${OUT_DIR}"
 
-OUTPUT_TXT=${IN_FILE%.*}-$(date +"%Y%m%d%H%M%S").txt
+OUTPUT_TXT="${OUT_DIR}/ffprobe-flat.txt"
 SCD_THRESHOLD=4.5
 
-CMD="ffprobe -report"
+export FFREPORT="file=${OUT_DIR}/ffprobe-report.log"
+CMD="ffprobe"
 CMD="${CMD} -f lavfi -i movie=${IN_FILE},scdet=threshold=${SCD_THRESHOLD},blackframe=amount=99:threshold=24,signalstats -show_entries frame=pkt_pts_time,width,height:frame_tags -print_format flat"
 echo ${CMD}
 echo "Output: ${OUTPUT_TXT}"
-${CMD}  > ${OUTPUT_TXT} 2> ${OUTPUT_TXT}-err.log
+${CMD}  > ${OUTPUT_TXT} 2> ${OUT_DIR}/ffprobe-output.log
