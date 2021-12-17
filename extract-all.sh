f8c710a22570c9e1504039b9111be16a814a2813
Ben Companjen <ben@companjen.name>
2021-12-17 18:31:28 +0100

    Expect extract-all to be called from Makefile
    - Determine video length automatically
    - Fail fast
    - Set path to video as subdir

--- extract-all.sh
+++ extract-all.sh
@@ -1,31 +1,33 @@
 #!/bin/sh
+set -e;
 
 VIDEO=$1
 FRAMES_CSV=$2
-LENGTH=$3
+FOLDER=$3
+LENGTH=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 ${FOLDER}/${VIDEO})
 
 number_of_frames=`wc -l ${FRAMES_CSV} | awk '{print $1}'`
 # echo ${number_of_frames}
 folders=${number_of_frames%[0-9][0-9][0-9][0-9]}
 # echo ${folders}
-iterations=$((${LENGTH} / 300))
+iterations=$((${LENGTH%.*} / 300))
 # echo $iterations
 # exit 0
 
 for f in $(seq -w 0 $folders); do
     for sf in $(seq -w 0 99); do
-        mkdir -p all_frames/${f}/${sf}
+        mkdir -p ${FOLDER}/all_frames/${f}/${sf}
     done
 done
-mkdir -p all_frames/tmp
+mkdir -p ${FOLDER}/all_frames/tmp
 # exit 0
 for START in $(seq 0 $iterations); do
     ST=$((${START} * 300))
     echo "Starting extraction at $ST seconds"
-    ffmpeg -hide_banner -loglevel level+quiet -ss ${ST}.0 -t 300.0 -i ${VIDEO} -vsync 0 -copyts -lavfi select='not(mod(n\,2))' -frame_pts true all_frames/tmp/f-%06d.jpg
+    ffmpeg -hide_banner -loglevel level+quiet -ss ${ST}.0 -t 300.0 -i ${FOLDER}/${VIDEO} -vsync 0 -copyts -lavfi select='not(mod(n\,3))' -frame_pts true ${FOLDER}/all_frames/tmp/f-%06d.jpg
     echo "Moving files"
     # Inspired by https://unix.stackexchange.com/a/97782/256251
-    for FILE in `ls all_frames/tmp`; do
+    for FILE in `ls ${FOLDER}/all_frames/tmp`; do
     # f="$(basename $FILE)"
     f="${FILE%.jpg}"
     numbers="${f#f-}"
@@ -33,7 +35,7 @@ for START in $(seq 0 $iterations); do
     firsttwo="${firstfour%[0-9][0-9]}"
     secondtwo="${firstfour#[0-9][0-9]}"
 
-    mv "all_frames/tmp/$FILE" "all_frames/${firsttwo}/${secondtwo}/"
+    mv "${FOLDER}/all_frames/tmp/$FILE" "${FOLDER}/all_frames/${firsttwo}/${secondtwo}/"
     done
     # parallel 'mv all_frames/tmp/f-{1}{2}??.jpg all_frames/{1}/{2}/' ::: {00..23} ::: {00..75}
 done
