d715accaca69e657f1758277438bb5c331569a69
Ben Companjen <ben@companjen.name>
2022-03-24 12:49:15 +0100

    Allow missing header in pivot
    Update scripts that call pivot.py

--- flat-to-csv.sh
+++ flat-to-csv.sh
@@ -8,4 +8,4 @@ PIVOTED_CSV=$(dirname $1)/frames-wide.csv
 echo "frame,variable,value" > ${OUT_CSV}
 sed -E -f $(dirname $0)/scripts/cleanup.txt ${IN_FILE} >> ${OUT_CSV}
 
-python3 $(dirname $0)/scripts/pivot.py ${OUT_CSV} ${PIVOTED_CSV}
+python3 $(dirname $0)/scripts/pivot.py -o ${PIVOTED_CSV} ${OUT_CSV}
--- scripts/pivot.py
+++ scripts/pivot.py
@@ -1,14 +1,33 @@
+"""
+Tidy a CSV file by pivoting
+"""
 import pandas as pd
-import sys
+import click
+import os.path
 
 
-stacked_file = sys.argv[1]
-stacked_data = pd.read_csv(stacked_file).fillna("")
-print(stacked_data.head())
+@click.command()
+@click.option('-o', '--output-file', type=click.Path(file_okay=True), required=False)
+@click.option('-m', '--header-missing', is_flag=True, help='Set to indicate '
+                'that the input file has no header line', required=False)
+@click.argument('input_file', type=click.Path(file_okay=True), required=True)
+def main(input_file, output_file, header_missing: bool):
+    """
+    Pivot a CSV file from 'frame,variable,value' format to tidy long format
+    """
+    if header_missing:
+        stacked_data = pd.read_csv(input_file, header=None, names=['frame','variable','value']).fillna("")
+    else:
+        stacked_data = pd.read_csv(input_file).fillna("")
+    print(stacked_data.head())
 
-pivoted_data = stacked_data.pivot(index="frame", columns="variable", values="value")
+    pivoted_data = stacked_data.pivot(index="frame", columns="variable", values="value")
 
-pivoted_file = stacked_file.replace(".csv", "-p.csv")
-if len(sys.argv) > 2:
-    pivoted_file = sys.argv[2]
-pivoted_data.to_csv(pivoted_file)
+    if output_file is None:
+        output_file = os.path.basename(input_file) + "-p.csv"
+
+    pivoted_data.to_csv(output_file)
+
+
+if __name__ == '__main__':
+    main()
